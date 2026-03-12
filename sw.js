f9f4544f3eb0f8fbb1b3ca877022d3c0b6558997
akhadjadj-glitch <ak.hadjadj@gmail.com>
2026-03-13 00:30:44 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,6 @@
-const CACHE_NAME = 'decor-pro-v1';
-const urlsToCache = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(names => Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))));
-  self.clients.claim();
-});
-
+const CACHE = 'decor-pro-v2';
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { self.clients.claim(); });
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-    if (!res || res.status !== 200) return res;
-    const clone = res.clone();
-    caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
-    return res;
-  })));
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
